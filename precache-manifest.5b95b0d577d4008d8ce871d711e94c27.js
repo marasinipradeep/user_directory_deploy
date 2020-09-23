c60391bddd4c30931808410a60bde01e7eac4d83
marasinipradeep <marasinipradeep@gmail.com>
2020-09-23 16:21:23 +0930

    Updates

---
+++ precache-manifest.5b95b0d577d4008d8ce871d711e94c27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75c4d33d293b9ac2ca327f1388e06d03",
+    "url": "/user_directory_deploy/index.html"
+  },
+  {
+    "revision": "764ebbe7c198ff0741f5",
+    "url": "/user_directory_deploy/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "5b4eb150137f8459cc44",
+    "url": "/user_directory_deploy/static/css/main.8b45d189.chunk.css"
+  },
+  {
+    "revision": "764ebbe7c198ff0741f5",
+    "url": "/user_directory_deploy/static/js/2.2a742c3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/user_directory_deploy/static/js/2.2a742c3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b4eb150137f8459cc44",
+    "url": "/user_directory_deploy/static/js/main.356a4d9a.chunk.js"
+  },
+  {
+    "revision": "f2601ffd756c82165859",
+    "url": "/user_directory_deploy/static/js/runtime-main.67c61e20.js"
+  }
+]);
